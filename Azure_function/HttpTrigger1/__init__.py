d27e091b97cda180347a8894db86ee8d463e4cb5
Jeroldjk <jerold.kingston77@gmail.com>
2022-11-26 01:16:37 +0100

    new azure

--- Azure_function/HttpTrigger1/__init__.py
+++ Azure_function/HttpTrigger1/__init__.py
@@ -7,27 +7,20 @@ def BlackBoxFunction(x):
     f = math.sin(x)
     return f
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    #logging.info('Python HTTP trigger function processed a request.')
-    start = float(0.0)
-    end = float(0.0)
     start = req.params.get('start')
     end = req.params.get('end')
-    name = req.params.get('name')
-    LENGTH = 0
-    WIDTH = 3.14
-    print(start)
-    print(end)
-    print({name})
     strr =" "
     strrr = " "
+    a = float(end)
+    b = float(start)
     N = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
     l = len(N)
     for i in range(0, l):
         integral = 0.0
-        dx = WIDTH-LENGTH/N[i]
+        dx = a-b/N[i]
         for j in range(1, N[i]):
-            xp = dx*(j+0.5)
-            di = BlackBoxFunction(xp)*dx
+            xp12 = dx*(j+0.5)
+            di = BlackBoxFunction(xp12)*dx
             integral += di
         strr += str(integral)+","
         strrr = strr
